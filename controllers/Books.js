7608af1925226d7918ecec749510aa81404ad762
Pravallika Mummadi <s555592@nwmissouri.edu>
2023-04-14 10:37:55 -0500

    edited

--- controllers/Books.js
+++ controllers/Books.js
@@ -12,8 +12,16 @@ exports.Books_list = async function (req, res) {
     }
 };
 // for a specific Books.
-exports.Books_detail = function (req, res) {
-    res.send('NOT IMPLEMENTED: Books detail: ' + req.params.id);
+exports.Books_detail = async function (req, res) {
+    //res.send('NOT IMPLEMENTED: Books detail: ' + req.params.id);
+    console.log("detail" + req.params.id)
+    try {
+        result = await Books.findById(req.params.id)
+        res.send(result)
+    } catch (error) {
+        res.status(500)
+        res.send(`{"error": document for id ${req.params.id} not found`);
+    }
 };
 
 
@@ -41,31 +49,43 @@ exports.Books_create_post = async function (req, res) {
 
 
 // Handle Books delete form on DELETE.
-exports.Books_delete = function (req, res) {
-    res.send('NOT IMPLEMENTED: Books delete DELETE ' + req.params.id);
+exports.Books_delete = async function (req, res) {
+   // res.send('NOT IMPLEMENTED: Books delete DELETE ' + req.params.id);
+   
+    console.log("delete " + req.params.id)
+    try {
+        result = await Books.findByIdAndDelete(req.params.id)
+        console.log("Removed " + result)
+        res.send(result)
+    } catch (err) {
+        res.status(500)
+        res.send(`{"error": Error deleting ${err}}`);
+    }
 };
+
 
 
 // Handle Books update form on PUT.
-exports.Books_update_put = function (req, res) {
-    res.send('NOT IMPLEMENTED: Books update PUT' + req.params.id);
+exports.Books_update_put = async function (req, res) {
+    //res.send('NOT IMPLEMENTED: Books update PUT' + req.params.id);
+    console.log(`update on id ${req.params.id} with body ${JSON.stringify(req.body)}`)
+    try {
+        let toUpdate = await Books.findById(req.params.id)
+        // Do updates of properties
+        if (req.body.Books_price) toUpdate.Books_price = req.body.Books_price;
+        if (req.body.Books_category)toUpdate.Books_category = req.body.Books_category;
+        if (req.body.Books_name) toUpdate.Books_name = req.body.Books_name;
+        if (req.body.Books ) toUpdate.Books  = req.body.Books ;
+        let result = await toUpdate.save();
+        console.log("Sucess " + result)
+        res.send(result)
+    } catch (err) {
+        res.status(500)
+        res.send(`{"error": ${err}: Update for id ${req.params.id} failed`);
+    }
 };
 
-// // List of all Books
-// exports.Books_list = async function(req, res) {
-//     try{
-//     theBooks = await Books.find();
-//     res.send(theBooks);
-//     }
-//     catch(err){
-//     res.status(500);
-//     res.send(`{"error": ${err}}`);
-//     }
-//    };
 
-
-// VIEWS
-// Handle a show all view
 exports.Book_view_all_Page = async function (req, res) {
     try {
         theBooks = await Books.find();
@@ -77,24 +97,31 @@ exports.Book_view_all_Page = async function (req, res) {
     }
 };
 
+// Handle building the view for updating a Bookss.
+// query provides the id
+exports.Books_update_Page =  async function(req, res) {
+    console.log("update view for item "+req.query.id)
+    try{
+        let result = await Books.findById(req.query.id)
+        res.render('Booksupdate', { title: 'Books Update', toShow: result });
+    }
+    catch(err){
+        res.status(500)
+        res.send(`{'error': '${err}'}`);
+    }
+};
 
-   // Handle Book create on POST.
-// exports.Book_create_post = async function(req, res) {
-//     console.log(req.body)
-//     let document = new Book();
-//     // We are looking for a body, since POST does not have query parameters.
-//     // Even though bodies can be in many different formats, we will be picky
-//     // and require that it be a json object
-//     // {"Book_type":"goat", "cost":12, "size":"large"}
-//     document.Book_type = req.body.Book_type;
-//     document.cost = req.body.cost;
-//     document.size = req.body.size;
-//     try{
-//     let result = await document.save();
-//     res.send(result);
-//     }
-//     catch(err){
-//     res.status(500);
-//     res.send(`{"error": ${err}}`);
-//     }
-//    };
+// Handle a delete one view with id from query
+exports.Books_delete_Page = async function(req, res) {
+    console.log("Delete view for id "  + req.query.id)
+    try{
+        result = await Books.findById(req.query.id)
+        res.render('Booksdelete', { title: 'Books Delete', toShow: result });
+    }
+    catch(err){
+        res.status(500)
+        res.send(`{'error': '${err}'}`);
+    }
+};
+
+   
