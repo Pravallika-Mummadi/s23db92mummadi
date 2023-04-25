60cf25622a07d2d890351e051e4ccff9ac4d6c50
Pravallika Mummadi <s555592@nwmissouri.edu>
2023-04-24 20:37:10 -0500

    edited

--- models/Books.js
+++ models/Books.js
@@ -1,13 +1,17 @@
 const mongoose = require("mongoose")
 const BookSchema = mongoose.Schema({
-    Books_price: Number,
+    Books_price: {
+        type:Number,
+    min: [3,"min value is 3"],
+    max: [1000,"max value is 1000"]
+},
     Books_category: {
         type: String,
 
     },
     Books_name: {
         type: String,
-
+        required : [true,"Books Name is required"]
     }
 
 })
--- routes/Books.js
+++ routes/Books.js
@@ -18,12 +18,25 @@ router.get('/', Books_controlers.Book_view_all_Page );
 /* GET detail Books page */
 router.get('/detail', Books_controlers.Books_view_one_Page);
 
-/* GET create Books page */
-router.get('/create', Books_controlers.Books_create_Page);
 
-router.get('/update', Books_controlers.Books_update_Page);
 /* GET delete Books page */
 router.get('/delete', Books_controlers.Books_delete_Page);
+// A little function to check if we have an authorized user and continue on
+// redirect to login.
+const secured = (req, res, next) => {
+ if (req.user){
+ return next();
+ }
+ req.session.returnTo = req.originalUrl;
+ res.redirect("/login");
+ }
+
+ router.get('/update',secured, Books_controlers.Books_update_Page);
+ /* GET create Books page */
+router.get('/create',secured, Books_controlers.Books_create_Page);
+
+/* GET delete Books page */
+router.get('/delete',secured, Books_controlers.Books_delete_Page);
 
 module.exports = router;
 // line 5 from Books.pug [  - let Book=[{Books_price:"234",Books_category :"Romcom",Books_name:"Beach Read"},{Books_price:"876",Books_category :"Thriller",Books_name:"Gone Girl"},{Books_price:"237",Books_category :"Fantasy",Books_name:"The Way of Kings"}]]
